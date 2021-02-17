50843641850fdd6516fbb310a4a3f93fbadfc12f
Hilary <hilary.koerner@gmail.com>
2021-02-16 18:55:59 -0600

    continue analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -8,11 +8,18 @@ employees.emp_no = salaries.emp_no;
 
 --#2: List the first name, last name, and hire date for employees who were hired in 1986
 select first_name, last_name, hire_date from employees
-where hire_date contains '%1986';
+where hire_date like '%1986';
 
 --#3: List the manager of each department with the following information: department number, 
 --department name, the manager's employee number, last name, first name
+--inner join three tables https://www.w3schools.com/sql/sql_join_inner.asp
+select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+from dept_manager
+inner join departments on
+departments.dept_no = dept_manager.dept_no
+inner join employees on 
+dept_manager.emp_no = employees.emp_no
 
-
+--#4
 
 

eb3a7880d878e50438d0fee345a249a54df5bbb4
Hilary <hilary.koerner@gmail.com>
2021-02-16 19:02:33 -0600

    analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -20,6 +20,14 @@ departments.dept_no = dept_manager.dept_no
 inner join employees on 
 dept_manager.emp_no = employees.emp_no
 
---#4
+--#4:  List the department of each employee with the following information: employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_manager
+inner join employees on
+employees.emp_no = dept_manager.emp_no
+inner join departments on 
+dept_manager.dept_no = departments.dept_no
 
-
+--#5: List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex from employees
+where first_name = 'Hercules' and last_name like '%B%';

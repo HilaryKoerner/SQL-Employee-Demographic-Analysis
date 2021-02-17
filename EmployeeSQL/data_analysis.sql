44f3b247ecf11d3c1729626d222116c5c35d28e5
Hilary <hilary.koerner@gmail.com>
2021-02-16 19:19:50 -0600

    completed analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -22,12 +22,38 @@ dept_manager.emp_no = employees.emp_no
 
 --#4:  List the department of each employee with the following information: employee number, last name, first name, and department name.
 select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-from dept_manager
+from dept_employee
 inner join employees on
-employees.emp_no = dept_manager.emp_no
+employees.emp_no = dept_employee.emp_no
 inner join departments on 
-dept_manager.dept_no = departments.dept_no
+dept_employee.dept_no = departments.dept_no
 
 --#5: List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 select first_name, last_name, sex from employees
 where first_name = 'Hercules' and last_name like '%B%';
+
+--#6: List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_employee
+inner join employees on
+employees.emp_no = dept_employee.emp_no
+inner join departments on 
+dept_employee.dept_no = departments.dept_no
+where dept_name = 'Sales';
+
+--#6: List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_employee
+inner join employees on
+employees.emp_no = dept_employee.emp_no
+inner join departments on 
+dept_employee.dept_no = departments.dept_no
+where dept_name = 'Sales' or dept_name = 'Development';
+
+--#8: In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+select last_name, count(last_name) as "Last Name Count"
+from Employees
+group by last_name
+order by "Last Name Count" desc;
